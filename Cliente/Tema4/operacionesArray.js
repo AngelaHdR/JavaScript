1fcdf6397edf83168a2767301aa5488734e4f0c4
Angela Hermosilla del Rio <angelahermosillad@gmail.com>
2024-09-26 13:42:10 +0200

    Operaciones con Array

---
+++ Cliente/Tema4/operacionesArray.js
@@ -0,0 +1,27 @@
+let miArray = [1, 4, 1, 4, 8, 9, 1, 8, 5];
+
+const eliminarDuplicados = () => {
+  let unicos = miArray.filter(
+    (num, i) => miArray.findIndex((b) => b == num) == i
+  );
+  return unicos;
+};
+
+const modificarNumeros = () => {
+  let numerosCuadrados = eliminarDuplicados().map((num) => {
+    return num ** 2;
+  });
+  return numerosCuadrados;
+};
+
+const sumaNumeros = () => {
+  const initial = 0;
+  const suma = modificarNumeros().reduce(
+    (accum, value) => accum + value,
+    initial
+  );
+  console.log(suma);
+  return suma;
+};
+
+sumaNumeros();
