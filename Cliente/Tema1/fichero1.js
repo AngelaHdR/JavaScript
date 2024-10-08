74d016c1a6fdfe82fefe18f74eff9ff5c7953edc
Angela Hermosilla del Rio <angelahermosillad@gmail.com>
2024-10-08 11:46:46 +0200

    Eliminar carpeta ejercicios clase

--- Cliente/Tema1/fichero1.js
+++
@@ -1,7 +0,0 @@
-let x=20;
-
-let textoX = document.createElement("h2");
-textoX.textContent = "Valor de x: " + x;
-
-let contenedorOperaciones = document.getElementById("operaciones");
-contenedorOperaciones.appendChild(textoX);
